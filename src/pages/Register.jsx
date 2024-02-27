ffe3e6239a534bb0f87169703d69e16b1c1368c6
ahmad244 <34864567+ahmad244@users.noreply.github.com>
2024-02-27 16:22:41 +0300

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html 	new file:   src/App.jsx 	new file:   src/components/Announcement.jsx 	new file:   src/components/Categories.jsx 	new file:   src/components/CategoryItem.jsx 	new file:   src/components/Footer.jsx 	new file:   src/components/Navbar.jsx 	new file:   src/components/Newsletter.jsx 	new file:   src/components/Product.jsx 	new file:   src/components/Products.jsx 	new file:   src/components/Slider.jsx 	new file:   src/data.js 	new file:   src/index.js 	new file:   src/pages/Cart.jsx 	new file:   src/pages/Home.jsx 	new file:   src/pages/Login.jsx 	new file:   src/pages/Product.jsx 	new file:   src/pages/ProductList.jsx 	new file:   src/pages/Register.jsx 	new file:   src/pages/Success.jsx 	new file:   src/redux/apiCalls.js 	new file:   src/redux/cartRedux.js 	new file:   src/redux/store.js 	new file:   src/redux/userRedux.js 	new file:   src/requestMethods.js 	new file:   src/responsive.js 	new file:   yarn.lock

---
+++ src/pages/Register.jsx
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+import { mobile } from "../responsive";
+import React, { useState } from "react";
+import axios from "axios";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: white;
+  ${mobile({ width: "75%" })}
+`;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // prevent default form submission behavior
+
+    // Check if passwords match before sending the request
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    try {
+      const response = await axios.post("localhost:5000/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      console.log(response.data); // You can use this data for feedback or redirect the user
+    } catch (error) {
+      console.error(error); // Handle errors, e.g., display an error message to the user
+    }
+  };
+  return (
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            placeholder="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Input
+            placeholder="confirm password"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <Agreement>
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button type="submit">CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
